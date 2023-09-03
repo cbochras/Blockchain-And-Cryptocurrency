bfe89fd4c62f789121f05db82fcfa470850b03b4
Cherifa Bochra Soltani <65483776+soltani-bochra@users.noreply.github.com>
2023-09-03 19:49:04 +0300

    Delete Decentralized Social Network directory

--- Decentralized Social Network/auto_gdb/krakenchain_dbg.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# use testnet settings,  if you need mainnet,  use ~/.krakenchain/krakenchaind.pid file instead
-export LC_ALL=C
-
-krakenchain_pid=$(<~/.krakenchain/testnet3/krakenchaind.pid)
-sudo gdb -batch -ex "source debug.gdb" krakenchaind ${krakenchain_pid}
